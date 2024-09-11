52c33ff54c859a161479f2930696d83418a0c1e0
r0-r0-r0 <ayoonro@gmail.com>
2024-09-11 16:01:42 +0200

    test

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 $(document).ready(function() {
   function splitTextIntoWords() {
       const $textElement = $('#interactive-text');
@@ -9,6 +8,22 @@ $(document).ready(function() {
       $textElement.html(wrappedWords);
   }
 
+  // Display text with line breaks
+  const textElement = document.getElementById('interactive-text');
+  const sentences = [
+      "I want to hide.",
+      "I fear being alone.",
+      "Terrified that you’ll see.",
+      "Afraid of your disappointment.",
+      "I struggle to breathe.",
+      "I struggle to be.",
+      "I long for love.",
+      "To hold your secrets close,",
+      "All the shame in me"
+  ];
+  textElement.innerHTML = sentences.join('                  '); // Adds line breaks between sentences
+
+  // Apply text wrapping and touch event handling
   splitTextIntoWords();
 
   // Add touch event handling
@@ -17,16 +32,15 @@ $(document).ready(function() {
   }).on('touchend', function() {
       $(this).css('filter', 'blur(5px)');
   });
-});
 
+  // Compatibility notice for Mobile Safari
+  function isMobileSafari() {
+      return /iP(hone|od|ad)/.test(navigator.userAgent) && !window.MSStream && /AppleWebKit/.test(navigator.userAgent);
+  }
 
-
-function isMobileSafari() {
-  return /iP(hone|od|ad)/.test(navigator.userAgent) && !window.MSStream && /AppleWebKit/.test(navigator.userAgent);
-}
-
-$(document).ready(function() {
   if (isMobileSafari()) {
       $('#compatibility-notice').show();
   }
 });
+
+
